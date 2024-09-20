f07c0631f7167d47d4b6be35e3a3e894e977c081
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2024-09-20 16:59:15 +0400

    initial commit

---
+++ src/main/java/de/gugaglonti/jollyapi/JollyApiApplication.java
@@ -0,0 +1,13 @@
+package de.gugaglonti.jollyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JollyApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JollyApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/gugaglonti/jollyapi/JollyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gugaglonti.jollyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JollyApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
